4a0782cca181400e227b2268a61b7555666abb3b
BGSERVER\USU <poatir@gmail.com>
2018-01-18 15:50:34 -0500

    B07 Puerta cierra y abre

---
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/ActorComponent.h"
+#include "Engine/TriggerVolume.h"
+#include "OpenDoor.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UOpenDoor : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOpenDoor();
+
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
+
+	void AbrirPuerta();
+	void CerrarPuerta();
+
+private:
+	AActor * DoorObject;
+
+	UPROPERTY(EditAnywhere)
+	float AnguloApertura=90.0f;
+
+	UPROPERTY(EditAnywhere)
+	ATriggerVolume * Trigger;
+
+	AActor * ActorQueAbre;
+
+	UPROPERTY(EditAnywhere)
+	float PuertaCerrarDelay = 5.f;
+
+	float UltimoTiempo;
+
+};
