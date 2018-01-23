11972d36add4097acefd44db8dbc230eb959ed6f
Instructor <poatir@gmail.com>
2018-01-23 13:03:18 -0500

    B09 Coger y soltar objetos
    Los objetos deben tener colisión y deben tener physics o de lo contrario no funciona el algoritmo

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -7,35 +7,115 @@
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	bWantsBeginPlay = true;
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+}
+void UGrabber::Grab()
+{
+	UE_LOG(LogTemp, Error, TEXT("Grab"));
+	auto HitResult = GetFirstPhysicsBodyInReach();
+	auto ComponentToGrab = HitResult.GetComponent();
+	auto ActorHit = HitResult.GetActor();
+	if (ActorHit)
+	{
+		PhysicsHandle->GrabComponent(
+			ComponentToGrab,
+			NAME_None,
+			ComponentToGrab->GetOwner()->GetActorLocation(),
+			true
+		);
+	}
+		
+}
+void UGrabber::Release()
+{
+	UE_LOG(LogTemp, Error, TEXT("Release"));
+	PhysicsHandle->ReleaseComponent();
 }
 
+void UGrabber::FindPhysicsHandleComponent()
+{
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Manejador de fisica correcto"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("%s missing physics handle component"), *GetOwner()->GetName());
+	}
+}
+
+void UGrabber::SetupInputComponent()
+{
+	if (!InputComponente)
+	{
+		InputComponente = GetOwner()->FindComponentByClass<UInputComponent>();
+		if (InputComponente)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Input component encontrado"));
+			InputComponente->BindAction("Grab_Mapping", IE_Pressed, this, &UGrabber::Grab);
+			InputComponente->BindAction("Grab_Mapping", IE_Released, this, &UGrabber::Release);
+		}
+		else
+		{
+			UE_LOG(LogTemp, Error, TEXT("Falta input component"));
+		}
+	}
+}
+
+const FHitResult UGrabber::GetFirstPhysicsBodyInReach()
+{
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PlayerViewPointLocation, PlayerViewPointRotation);
+	UE_LOG(LogTemp, Warning, TEXT("Posición: %s Rotación: %s"), *PlayerViewPointLocation.ToString(),*PlayerViewPointRotation.ToString());
+	FVector LineaTrazada = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * 100.0f;
+	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineaTrazada, FColor(255, 0, 0), false, 0.0f, 0.0f, 10.f);
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineaTrazada,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParameters
+	);
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("LineTraceHit: %s"), *(ActorHit->GetName()));
+	}
+	return Hit;
+}
 
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
-	// ...
+	FindPhysicsHandleComponent();
 	
 }
-
-
 // Called every frame
 void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
 {
 	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
-	FVector Position;
-	FRotator Rotacion;
-	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(Position, Rotacion);
-	UE_LOG(LogTemp, Warning, TEXT("Posición: %s Rotación: %s"), *Position.ToString(),*Rotacion.ToString());
-	FVector LineaTrazada = Position + Rotacion.Vector() * 100.0f;
-	DrawDebugLine(GetWorld(),Position,LineaTrazada,FColor(255,0,0),false,0.0f,0.0f,10.f);
-	// ...
+	if (UnaVez == false)
+	{
+		SetupInputComponent();
+		UnaVez = true;
+	}
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PlayerViewPointLocation, PlayerViewPointRotation);
+	//UE_LOG(LogTemp, Warning, TEXT("Posición: %s Rotación: %s"), *PlayerViewPointLocation.ToString(),*PlayerViewPointRotation.ToString());
+	FVector LineaTrazada = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * 200.0f;
+	if (PhysicsHandle->GrabbedComponent)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Objeto cogido"));
+		PhysicsHandle->SetTargetLocation(LineaTrazada);
+	}
+
 }
 
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -3,6 +3,10 @@
 #pragma once
 
 #include "Components/ActorComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
+
+
 #include "Grabber.generated.h"
 
 
@@ -11,16 +15,24 @@ class BUILDINGESCAPE_API UGrabber : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UGrabber();
 
 	// Called when the game starts
 	virtual void BeginPlay() override;
-	
-	// Called every frame
-	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
-	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+	UInputComponent* InputComponente = nullptr;
+	void Grab();
+	void Release();
+	void FindPhysicsHandleComponent();
+	void SetupInputComponent();
+	const FHitResult GetFirstPhysicsBodyInReach();
+	bool UnaVez = false;
+
 };
